581289bf9459d88e6b48082a6f64be0124516ff6
tcma <b93901190@g.ntu.edu.tw>
2018-07-06 05:02:32 +0000

    add style, new form, and back page

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+var campgrounds = [
+        {name: "Salmon Creek", image: "https://pixabay.com/get/e834b70c2cf5083ed1584d05fb1d4e97e07ee3d21cac104496f0c679a2ecb5bc_340.jpg"},
+        {name: "Granite Hill", image: "https://pixabay.com/get/e83db30820f7033ed1584d05fb1d4e97e07ee3d21cac104496f0c679a2ecb5bc_340.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e831b20628f2003ed1584d05fb1d4e97e07ee3d21cac104496f0c679a2ecb5bc_340.jpg"}
+    ];
 
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -10,14 +18,25 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function (req, res) {
-    var campgrounds = [
-            {name: "Salmon Creek", image: "https://pixabay.com/get/e834b70c2cf5083ed1584d05fb1d4e97e07ee3d21cac104496f0c679a2ecb5bc_340.jpg"},
-            {name: "Granite Hill", image: "https://pixabay.com/get/e83db30820f7033ed1584d05fb1d4e97e07ee3d21cac104496f0c679a2ecb5bc_340.jpg"},
-            {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e831b20628f2003ed1584d05fb1d4e97e07ee3d21cac104496f0c679a2ecb5bc_340.jpg"}
-        ];
-    
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+
+app.post("/campgrounds", function(req, res){
+    // get data from form and add to campgrounds array
+    // redirect back to campgrounds page
+    // res.send("post!!")
+    var campName = req.body.name;
+    var imgUrl = req.body.image;
+    var newCamp = {name: campName, image: imgUrl};
+    campgrounds.push(newCamp);
     
+    res.redirect("/campgrounds");
+    
+});
+
+app.get("/campgrounds/new", function(req, res) {
+   res.render("new.ejs");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
